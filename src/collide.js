348093ca68eddd7fb9df8b2726a1f06d7d578558
birdroid <birdroid@126.com>
2013-07-09 10:28:41 +0800

    粗略完成collide test

--- src/collide.js
+++ src/collide.js
@@ -1,5 +1,11 @@
 define(function(require, exports, module){
   exports.isRectCollide = function(v1,v2){
-  //to do
+      //比较包含矩形的最小矩形
+      if(v2.maxX > v1.minX && v2.minX< v1.maxX && v1.maxY>v2.minY && v1.minY < v2.maxY){
+          return true;
+      }else{
+          return false;
+      }
+      //todo:叉积判断矩形是否相交
   };
 });
